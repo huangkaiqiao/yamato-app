be27063f206ca618744a4a01b69a5c42b45a80f9
HongRyeon <huangkaiqiao@gmail.com>
2022-09-19 13:52:51 +0800

    2022-09-18

--- lib/screen/mine.dart
+++ lib/screen/mine.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -7,21 +9,59 @@ class MineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-    Widget draggable = Column(
-        children: [
-          const Image(
-              image: AssetImage('images/arc.png'),
-              width: 360,
-              // height: 20,
-              fit: BoxFit.contain
-          ),
-          Container(
-            width: 360,
-            height: 100,
-            color: const Color(0xFFF9F9F9),
-          ),
-        ]
+    Widget draggable = SizedBox(height: 300,
+      child: Column(
+          children: [
+            const Image(
+                image: AssetImage('images/arc.png'),
+                width: 360,
+                // height: 20,
+                fit: BoxFit.contain
+            ),
+            Container(
+              // alignment: Alignment.center,
+                width: 360,
+                padding: const EdgeInsets.all(10),
+                height: 100,
+                // color: const Color(0xFFF9F9F9),
+                color: Colors.black,
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  width: 340,
+                  color: Colors.white,
+                  child: Flex(
+                    direction: Axis.vertical,
+                    children: const [
+                      Text('1'),
+                      Text('余额豆', style: TextStyle(color: Colors.black38, fontSize: 12),)
+                    ],
+                  ),
+                )
+            ),
+            Container(
+              // alignment: Alignment.center,
+                width: 360,
+                // height: 100,
+                color: const Color(0xFFF9F9F9),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  width: 340,
+                  color: Colors.white,
+                  child: Flex(
+                    direction: Axis.vertical,
+                    children: const [
+                      Text('2'),
+                      Text('余额豆', style: TextStyle(color: Colors.black38, fontSize: 12),)
+                    ],
+                  ),
+                )
+            ),
+          ]
+      )
     );
+
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -43,6 +83,7 @@ class MineScreen extends StatelessWidget {
           statusBarBrightness: Brightness.dark,
         ))
       ),
+      backgroundColor: const Color(0xfff5f5f5),
       body: Stack(
         children: [
           Container(
@@ -59,10 +100,12 @@ class MineScreen extends StatelessWidget {
               child: Stack(
                 children: [
                   Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
-                        width: 100.0,
-                        height: 100.0,
+                        margin: const EdgeInsets.only(left: 10, top: 55),
+                        width: 60.0,
+                        height: 60.0,
                         decoration: BoxDecoration(
                           color: const Color(0xff7c94b6),
                           image: const DecorationImage(
@@ -73,15 +116,18 @@ class MineScreen extends StatelessWidget {
                           borderRadius: const BorderRadius.all( Radius.circular(50.0)),
                           border: Border.all(
                             color: Colors.white,
-                            width: 4.0,
+                            width: 2.0,
                           ),
                         ),
                       ),
-                      Column(
-                        children: const [
-                          Text('海顺汽车经销商'),
-                          Text('139****1111'),
-                        ],
+                      Container(
+                        margin: const EdgeInsets.only(left: 10, top: 65),
+                        child: Column(
+                          children: const [
+                            Text('海顺汽车经销商'),
+                            Text('139****1111'),
+                          ],
+                        )
                       )
                     ],
                   ),
@@ -93,31 +139,37 @@ class MineScreen extends StatelessWidget {
             child: Draggable(
               axis: Axis.vertical,
               data: 10,
-              feedback: draggable,
+              feedback: Material(color: Colors.transparent, child: draggable),
               childWhenDragging: Container(),
-              maxSimultaneousDrags: 50,
+              // maxSimultaneousDrags: 10,
               child: draggable,
             )
           ),
-          Positioned(
-            top: 200,
-            child: DragTarget<int>(
-              builder: (BuildContext context, List<dynamic> accepted, List<dynamic> rejected,) {
-                  return Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.cyan,
-                    child: const Center(
-                      child: Text('Value is updated to:'),
-                    ),
-                  );
-                  },
-                onAccept: (int data) {
-                  // setState(() {
-                  //   acceptedData += data;
-                  // });
-                },
-              ),)
+          // Positioned(
+          //   top: 200,
+          //   child: DragTarget<int>(
+          //     builder: (BuildContext context, List<dynamic> accepted, List<dynamic> rejected,) {
+          //         return Container(
+          //           height: 100.0,
+          //           width: 100.0,
+          //           color: Colors.cyan,
+          //           child: Center(
+          //             child: Column(
+          //               children: const [
+          //                 Text('0'),
+          //                 Text('余额豆')
+          //               ]
+          //             ),
+          //           ),
+          //         );
+          //         },
+          //       onAccept: (int data) {
+          //         // setState(() {
+          //         //   acceptedData += data;
+          //         // });
+          //       },
+          //     ),
+          // )
         ],
       )
     );
